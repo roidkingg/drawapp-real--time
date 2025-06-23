e1740dbbefd8a43eaada351e25e6cf8bac7c1592
Aryan <rarharyan@gmail.com>
2025-06-23 19:03:18 +0530

    rrtgsfz

--- apps/excelidraw-frontend/config.ts
+++ apps/excelidraw-frontend/config.ts
@@ -1,3 +1,2 @@
-
-export const HTTP_BACKEND = "http://ec2-54-205-229-113.compute-1.amazonaws.com:3001"
-export const WS_URL = "ws://ec2-54-205-229-113.compute-1.amazonaws.com:8080";
+export const HTTP_BACKEND = "https://http-backend.aryanrarh.dev"
+export const WS_URL = "wss://ws-backend.aryanrarh.dev";
