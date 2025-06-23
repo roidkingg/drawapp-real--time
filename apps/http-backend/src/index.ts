652143c495720145c4426e11439d73743eb78c18
Aryan <rarharyan@gmail.com>
2025-06-23 22:12:15 +0530

    rfea

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.post("/signup", async (req, res) => {
-
     const parsedData = CreateUserSchema.safeParse(req.body);
     if (!parsedData.success) {
         console.log(parsedData.error);
